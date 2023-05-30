ba8690bf823513a25fdd796205a8e16239043bb3
Yosef Dereje <Yosefdereje19@gmail.com>
2023-05-30 04:42:22 -0700

    from task 9 to 16

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -1,42 +1,24 @@
 #include <stdio.h>
 /**
-  * main - print a table of the first 178 fibonacci numbers.
+  * main - sum even fibonacci numbers under 4 million.
   * Return: Nothing.
   */
 int main(void)
 {
-	int count;
-	unsigned long i, j, k;
-	unsigned long ia, ib, ja, jb, ka, kb, carry;
-	unsigned long amount;
+	unsigned long count, i, j, k, sums;
 
-	count = 0;
-	i = 0;
+	i = sums = 0;
 	j = 1;
-	amount = 10000000000000000000;/*10^18*/
-	for (count = 1; count <= 91; count++)
+	for (count = 0; count < 50; count++)
 	{
 		k = i + j;
 		i = j;
 		j = k;
-		printf("%d %lu\n", count + 1, k);
+		if (k % 2 == 0 && k < 4000000)
+		{
+			sums += k;
+		}
 	}
-	ia = i / amount;
-	ib = i % amount;
-	ja = j / amount;
-	jb = j % amount;
-	while (count <= 178)
-	{
-		carry = (ib + jb) / amount;
-		ka = (ia + ja) + carry;
-		kb = (ib + jb ) - carry * amount;
-		ia = ja;
-		ib = jb;
-		ja = ka;
-		jb = kb;
-		printf("%d %lu%018lu\n", count + 1, ka, kb);
-		count++;
-	}
-	printf("%lu018", ka);
+	printf("%lu\n", sums);
 	return (0);
 }
