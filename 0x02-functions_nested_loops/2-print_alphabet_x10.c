ede8193bb8c107eeb75803965823280b69664ea6
Yosef Dereje <Yosefdereje19@gmail.com>
2023-05-29 23:28:59 -0700

    10 x alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,15 +1,19 @@
 #include "main.h"
 /**
- * print_alphabet - prints the english alphabet from a-z.
- * Return: Nothing.
- */
-void print_alphabet(void)
+  * print_alphabet_x10 - repeats the print_alphabet 10 times.
+  * Return: Nothing.
+  */
+void print_alphabet_x10(void)
 {
+	int i;
 	char c;
 
-	for (c = 'a'; c <= 'z'; c++)
+	for (i = 0; i < 10; ++i)
 	{
-		_putchar(c);
+		for (c = 'a'; c <= 'z'; c++)
+		{
+			_putchar(c);
+		}
+		_putchar('\n');
 	}
-	_putchar('\n');
 }
